c67897a04284856d6c17ca36db36ee837695a7f7
Heli Eronen <herojonen@gmail.com>
2023-07-06 16:29:44 +0300

    Tehtävä 1.11

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,20 +1,27 @@
 import { useState } from 'react'
 
-const Button = ({ handleClick, text}) => (
-  <button onClick={ handleClick }>{text}</button>
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
 )
 
-const StatisticsLine = ({ text, value }) => (
-  <p>{text} {value}</p>
-)
+const StatisticsLine = ({ text, value }) => {
+  return (
+    <tbody>
+      <tr>
+        <td>{text}</td>
+        <td>{value}</td>
+      </tr>
+    </tbody>
+  )
+}
 
 const Statistics = ({ good, neutral, bad }) => {
 
   const getAll = () => good + neutral + bad
-  const getAverage = () => (good*1 + neutral*0 + bad*(-1)) / getAll()
+  const getAverage = () => (good * 1 + neutral * 0 + bad * (-1)) / getAll()
   const getPositive = () => (good / getAll()) * 100
 
-  if ( getAll() === 0) {
+  if (getAll() === 0) {
     return (
       <p>No feedback given</p>
     )
@@ -23,12 +30,14 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <>
       <h2>Statistics</h2>
-      <StatisticsLine text="good" value={ good }/>
-      <StatisticsLine text="neutral" value={ neutral }/>
-      <StatisticsLine text="bad" value={ bad }/>
-      <StatisticsLine text="all" value={ getAll() }/>
-      <StatisticsLine text="average" value={ getAverage() }/>
-      <StatisticsLine text="positive" value={ getPositive() + " %" }/>
+      <table>
+        <StatisticsLine text="good" value={good} />
+        <StatisticsLine text="neutral" value={neutral} />
+        <StatisticsLine text="bad" value={bad} />
+        <StatisticsLine text="all" value={getAll()} />
+        <StatisticsLine text="average" value={getAverage()} />
+        <StatisticsLine text="positive" value={getPositive() + " %"} />
+      </table>
     </>
   )
 }
@@ -38,7 +47,7 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  
+
 
   return (
     <div>
@@ -46,7 +55,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <Statistics good={ good } neutral={ neutral } bad={ bad } />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }

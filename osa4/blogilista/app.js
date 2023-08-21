20d550aa7f63ac5f91182992f3061a9185109dda
Heli Eronen <herojonen@gmail.com>
2023-08-21 13:53:48 +0300

    Tehtävä 4.15

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -6,6 +6,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -24,6 +25,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.errorHandler)
 

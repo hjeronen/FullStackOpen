dfd4fe698dd1d851566226cecd55977d0d63af6c
Heli Eronen <herojonen@gmail.com>
2023-08-22 12:39:19 +0300

    Tehtävä 4.18

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -7,6 +7,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
@@ -26,6 +27,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 

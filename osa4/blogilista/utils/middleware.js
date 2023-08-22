773cd747b58cb35659d2b2e9ed7b7bc08686a644
Heli Eronen <herojonen@gmail.com>
2023-08-22 13:07:55 +0300

    Tehtävä 4.19

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -4,6 +4,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: error.message })
   } else if (error.name === 'ValidationError') {
     return response.status(400).send({ error: error.message })
+  } else if (error.name ===  'JsonWebTokenError') {
+    return response.status(400).json({ error: 'token missing or invalid' })
   }
 
   next(error)

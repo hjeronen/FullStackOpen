3eaa80a4a26a3479ec42529a89b1c7acd6e8bb7f
Heli Eronen <herojonen@gmail.com>
2023-08-30 15:31:45 +0300

    Tehtävä 5.16

--- osa5/bloglist-frontend/src/components/BlogForm.jsx
+++ osa5/bloglist-frontend/src/components/BlogForm.jsx
@@ -32,6 +32,7 @@ const BlogForm = ({ createBlog }) => {
           type="text"
           value={title}
           name="Title"
+          id="title"
           onChange={({ target }) => setTitle(target.value)}
         />
       </div>
@@ -41,6 +42,7 @@ const BlogForm = ({ createBlog }) => {
           type="text"
           value={author}
           name="Author"
+          id="author"
           onChange={({ target }) => setAuthor(target.value)}
         />
       </div>
@@ -50,6 +52,7 @@ const BlogForm = ({ createBlog }) => {
           type="text"
           value={url}
           name="Url"
+          id="url"
           onChange={({ target }) => setUrl(target.value)}
         />
       </div>
---
+++ osa5/bloglist-frontend/src/components/BlogForm.test.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import BlogForm from './BlogForm'
+
+const blog = {
+  title: 'Jest Test Blog',
+  author: 'Jest Tester',
+  url: 'http://fakeurl.org'
+}
+
+test(
+  'when creating a blog mockhandler gets called with correct data',
+  async () => {
+    const user = userEvent.setup()
+    const createBlog = jest.fn()
+
+    const { container } = render(<BlogForm createBlog={createBlog} />)
+
+    const titleInput = container.querySelector('#title')
+    const authorInput = container.querySelector('#author')
+    const urlInput = container.querySelector('#url')
+
+    const createButton = screen.getByText('create')
+
+    await user.type(titleInput, blog.title)
+    await user.type(authorInput, blog.author)
+    await user.type(urlInput, blog.url)
+    await user.click(createButton)
+
+    expect(createBlog.mock.calls).toHaveLength(1)
+
+    const content = createBlog.mock.calls[0][0]
+    expect(content.title).toBe(blog.title)
+    expect(content.author).toBe(blog.author)
+    expect(content.url).toBe(blog.url)
+  })

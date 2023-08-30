4876728608cf9ae08a2d01fb7f61ab62dd93d3b0
Heli Eronen <herojonen@gmail.com>
2023-08-30 13:52:29 +0300

    Tehtävä 5.14

--- osa5/bloglist-frontend/src/components/Blog.test.js
+++ osa5/bloglist-frontend/src/components/Blog.test.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
+const updateBlog = jest.fn()
+const deleteBlog = jest.fn()
+
+const user = {
+  username: 'testuser',
+  name: 'Test User',
+  id: '012345'
+}
+
+const blog = {
+  title: 'Jest Test Blog',
+  author: 'Jest Tester',
+  url: 'http://fakeurl.org',
+  likes: 5,
+  user: user
+}
+
 test('only title and author are rendered', () => {
-  const updateBlog = jest.fn()
-  const deleteBlog = jest.fn()
-
-  const user = {
-    username: 'testuser',
-    name: 'Test User',
-    id: '012345'
-  }
-
-  const blog = {
-    title: 'Jest Test Blog',
-    author: 'Jest Tester',
-    url: 'http://fakeurl.org',
-    user: user
-  }
-
   render(
     <Blog
       blog={blog}
@@ -39,4 +41,36 @@ test('only title and author are rendered', () => {
 
   const userName = screen.queryByText(blog.user.name)
   expect(userName).toBeNull()
+})
+
+test('other blog info is rendered after pressing the button', async () => {
+  render(
+    <Blog
+      blog={blog}
+      updateBlog={updateBlog}
+      deleteBlog={deleteBlog}
+      user={user} />
+  )
+
+  const urlBefore = screen.queryByText(blog.url)
+  expect(urlBefore).toBeNull()
+  
+  const likesBefore = screen.queryByText(`likes: ${blog.likes}`)
+  expect(likesBefore).toBeNull()
+
+  const userNameBefore = screen.queryByText(blog.user.name)
+  expect(userNameBefore).toBeNull()
+
+  const clicker = userEvent.setup()
+  const button = screen.getByText('view')
+  await clicker.click(button)
+
+  const urlAfter = screen.queryByText(blog.url)
+  expect(urlAfter).toBeDefined()
+  
+  const likesAfter = screen.queryByText(`likes: ${blog.likes}`)
+  expect(likesAfter).toBeDefined()
+
+  const userNameAfter = screen.queryByText(blog.user.name)
+  expect(userNameAfter).toBeDefined()
 })
